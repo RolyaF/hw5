f299378d34110906380fae294d42e0fd53b8d9e9
Railya <fazyllinarailya@gmail.com>
2023-05-01 21:08:36 +0300

    commit

---
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class RestServiceTest {
+
+    @Test
+    public void calculateTest1() {
+        RestService service = new RestService();
+
+        int actual = service.calculate(10000, 3000);
+        int expected = 3;
+
+        Assertions.assertEquals(actual, expected);
+    }
+    @Test
+    public void calculateTest2() {
+        RestService service = new RestService();
+
+        int actual = service.calculate(100000, 60000);
+        int expected = 2;
+
+        Assertions.assertEquals(actual, expected);
+    }
+}
