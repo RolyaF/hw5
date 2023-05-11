21ddd9e2efca3413d545201c9b47a3cb343034c9
Railya <fazyllinarailya@gmail.com>
2023-05-11 08:50:36 +0300

    commit

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -9,7 +9,7 @@ class RestServiceTest {
     public void calculateTest1() {
         RestService service = new RestService();
 
-        int actual = service.calculate(10000, 3000);
+        int actual = service.calculate(10000, 3000, 20000);
         int expected = 3;
 
         Assertions.assertEquals(actual, expected);
@@ -18,7 +18,7 @@ class RestServiceTest {
     public void calculateTest2() {
         RestService service = new RestService();
 
-        int actual = service.calculate(100000, 60000);
+        int actual = service.calculate(100000, 60000, 150000);
         int expected = 2;
 
         Assertions.assertEquals(actual, expected);
